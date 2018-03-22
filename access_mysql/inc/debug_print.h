04225c8b7b88c91a4b4ca094f197b37135ea0fa3
punksnail <punksnail@outlook.com>
2018-03-22 11:37:12 +0800

    delete debug print platform info

--- access_mysql/inc/debug_print.h
+++ access_mysql/inc/debug_print.h
@@ -2,25 +2,6 @@
 #ifndef DEBUG_PRINT_HHH
 #define DEBUG_PRINT_HHH
 
-
-
-#ifdef _WIN32
-
-#ifdef _DEBUG
-#define debug_print(...)\
-    do{\
-        fprintf(stderr, __VA_ARGS__);\
-    }while(0)
-#else
-#define debug_print(...)\
-    do{  }while(0)
-#endif
-
-#endif
-
-
-#ifdef linux 
-
 #ifdef _DEBUG
 #define debug_print(...)\
     do{\
@@ -31,9 +12,6 @@
 #define debug_print(...)\
     do{ }while(0)
 #endif
-
-#endif
-
 
 
 #endif
