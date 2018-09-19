cbe8670ba6a31eb5b012011293b4084d98b3ca17
punksnail <punksnail@outlook.com>
2018-09-19 17:48:40 +0800

    add description of the complexity

--- classic_sort/inc/classic_sort.h
+++ classic_sort/inc/classic_sort.h
@@ -9,16 +9,22 @@
 extern "C" {
 #endif
 
+/* average: O(n^2)  best: O(n)  worst: O(n^2) */
 void bubble_sort(int *array, int len);
 
+/* average: O(n^2)  best: O(n^2)    worst: O(n^2) */
 void select_sort(int *array, int len);
 
+/* average: O(n^2)  best: O(n)  worst: O(n^2) */
 void insert_sort(int *array, int len);
 
+/* average: O(nlogn)    best: O(nlogn)  worst: O(n^2) */
 void quick_sort(int *array, int len);
 
+/* average: O(nlog2n)   best: O(nlogn)  worst: O(n^2) */
 void shell_sort(int *array, int len);
 
+/* average: O(nlogn)    best: O(nlogn)  worst: O(nlogn) */
 void merge_sort(int *array, int len);
 
 #ifdef __cplusplus
