a66877203e447b088b7267a93db0540f1b488e2d
punksnail <punksnail@outlook.com>
2019-12-25 11:50:55 +0800

    添加无中间变量交换指针的宏

--- boring_tools/inc/boring_tools.h
+++ boring_tools/inc/boring_tools.h
@@ -5,8 +5,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* if "order" is false, output with reverse order
- * else, output with memory order
+#define SWAP_PTR(p1, p2) \
+    p1 = (void*)((uint64_t)p1 ^ (uint64_t)p2); \
+    p2 = (void*)((uint64_t)p1 ^ (uint64_t)p2); \
+    p1 = (void*)((uint64_t)p1 ^ (uint64_t)p2);
+
+/* if "order" is false, output in reverse order
+ * otherwise, output in memory order
  */
 void show_fake_bits(const void *p_value, uint32_t value_len, bool order);
 
--- boring_tools/src/main_test.c
+++ boring_tools/src/main_test.c
@@ -2,20 +2,38 @@
 #include <stdio.h>
 #include "boring_tools.h"
 
+#define line() printf("\n------------------------------\n");
+
 void test_show_fake_bits(void)
 {
     uint32_t value = 3;
     printf("uint32_t \"%u\" reverse order:\n", value);
     show_fake_bits(&value, sizeof(value), false);
     
-    char buf[10] = "abc";
+    char buf[] = "abc";
 
     printf("string \"%s\" memory order:\n", buf);
     show_fake_bits(buf, sizeof(buf), true);
 }
 
+void test_swp_ptr(void)
+{
+    const char *a = "this is a string";
+    const char *b = "this is b string";
+
+    printf("\na: %s\tb: %s\n", a, b);
+
+    printf("swap:");
+
+    SWAP_PTR(a, b);
+
+    printf("\na: %s\tb: %s\n", a, b);
+}
+
 int main(int argc, char *argv[])
 {
+    test_swp_ptr();
+    line();
     test_show_fake_bits();
 
     return 0;
